eebd2eef6731ec9bed17b0d22d0df9d2479f51b6
ParthSharma00700 <captainparth007@gmail.com>
2023-03-26 12:15:48 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYh-VQ1cm7H2EQKMW_0YrS1ir7LGtA89w",
+    authDomain: "netflix-clone-fallnxt.firebaseapp.com",
+    projectId: "netflix-clone-fallnxt",
+    storageBucket: "netflix-clone-fallnxt.appspot.com",
+    messagingSenderId: "217171270702",
+    appId: "1:217171270702:web:9f968d84de7a0a104bd3bf"
+  };
