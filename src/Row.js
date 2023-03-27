8136357d531c85de4626f64425508771d6bf9ff5
tarundhiman85 <tarundhiman0047@gmail.com>
2023-03-27 12:16:21 +0530

    added banner and rows

--- src/Row.js
+++ src/Row.js
@@ -4,9 +4,9 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl, isLargeRow }) {
+function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
-
+  const base_url = "https://image.tmdb.org/t/p/original/"
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -15,17 +15,28 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }
     fetchData();
   }, [fetchUrl]);
-
+  // console.log(movies)
   return (
     <div className="row">
-      
-      <h2>{title}</h2>
-
-    
-    
-
-      
-
+      <h2>{title}</h2>     
+      <div className="row__posters"> 
+      {
+      movies.map(
+        (movie) => (
+        ((isLargeRow && movie.poster_path) ||
+        (!isLargeRow && movie.backdrop_path) && 
+              <img
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              key={movie.id}
+              src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path  
+              }`}
+              alt={movie.name}
+              />
+           )
+        )
+      )}  
+      </div>
     </div>
   );
 }
